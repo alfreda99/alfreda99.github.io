8fa614b08a3b5dcc29e7c761b3751f491c534063
Alfreda <alfredadownie@gmail.com>
2015-03-11 00:04:11 -0400

    reverted element creation back inside for loop

--- views/js/main.js
+++ views/js/main.js
@@ -530,18 +530,14 @@ document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
   var movingPizzas = document.getElementById("movingPizzas1");
-  var elem = document.createElement('img');
-  elem.className = 'mover';
-  elem.src = "images/pizza.png";
-  elem.style.height = "100px";
-  elem.style.width = "73.333px";
+  var elem = "";
 
   for (var i = 0; i < 200; i++) {
-   /* elem = document.createElement('img');
+    elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
-    elem.style.width = "73.333px";*/
+    elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     movingPizzas.appendChild(elem);
