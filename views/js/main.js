88904970273492c23147cf361c588cabb3afb271
Alfreda <alfredadownie@gmail.com>
2015-03-11 23:55:14 -0400

    reduced to 20 pizzas

--- views/js/main.js
+++ views/js/main.js
@@ -501,11 +501,11 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 
 // Moves the sliding background pizzas based on scroll position
 function updatePositions() {
-  window.performance.mark("mark_start_frame");
   frame++;
   var phase = 0;
   var scrolltop = Math.sin(document.body.scrollTop / 1250);
   var items = document.getElementById("movingPizzas1").getElementsByClassName("mover");
+  window.performance.mark("mark_start_frame");
 
   for (var i = 0; i < items.length; i++) {
     phase = scrolltop + (i % 5);
@@ -532,7 +532,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var elem = "";
   var movingPizzas = document.getElementById("movingPizzas1");
 
-  for (var i = 0; i < 50; i++) {
+  for (var i = 0; i < 20; i++) {
     elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
